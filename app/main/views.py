52814f01ca02a283a32bad8e242249c7f2cf9ebd
Doktatech <Doktatech2@gmail.com>
2019-06-02 21:43:19 +0300

    Declare the logic for handling the root template

---
+++ app/main/views.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from . import main
+import datetime
+from flask import render_template, request, redirect, url_for, abort, flash
+from flask_login import login_required
+from ..models import User, Post, Comment
+from .forms import UpdateProfile, PostForm , CommentForm
+from .. import db, photos
+app = Flask(__name__)
+
+
+# views
+@main.route("/")
+def index():
+   '''
+   title = "Post Perfect"
+   '''
+   title = 'Post Perfect'
+   posts = Post.query.all()
+
+   return render_template('index.html', title= title, posts = posts)
